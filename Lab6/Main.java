dd778ff0381788fed7a93823badd5dbce87b4add
Yaroslav Zholob <73608684+ZholobYaroslav@users.noreply.github.com>
2021-11-29 20:47:13 +0200

    Create Main.java

---
+++ Lab6/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pizzeria pizzeria = new Pizzeria();
+        pizzeria.callMenu();
+    }
+}
